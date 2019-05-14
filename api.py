97c5730e457602c25b044b51be6d84a2c878538b
Kallan <kallan@locationequipment.com.au>
2019-05-14 16:44:43 +0800

    Initial attempt at zappa

--- api.py
+++ api.py
@@ -6,13 +6,11 @@ import rmshelper.rmshelper as rmshelper
 api = Flask(__name__)
 
 
-@api.route("/quick_invoice/<int:opportunity_id>/")
+@api.route("/quick_invoice/<int:opportunity_id>/", methods=["GET"])
 def quick_invoice(opportunity_id):
     invoice = rmshelper.quick_invoice(opportunity_id)
     if invoice["post_invoice_status_code"] == 200:
-        resp = jsonify(invoice)
-        resp.status_code = 200
-        return resp
+        return jsonify(invoice), 200
 
 
 if __name__ == "__main__":
